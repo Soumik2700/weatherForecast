ea43afe89b7580c8300d5b130ac2b4849ce43e40
Soumik Sinha <soumiksingha8@gmailcom>
2024-10-21 22:08:14 +0530

    added day1 weather

--- src/script.js
+++ src/script.js
@@ -13,6 +13,61 @@ darkModeBtn.addEventListener("click", function () {
 
 const apiKey = '86cf74ee20fbf270b000f8322804dcae';
 const latLonBtn = document.getElementById("latLonBtn");
+const searchCityBtn = document.getElementById("searchCity");
+// console.log(searchCityBtn);
+
+searchCityBtn.addEventListener("click", getDataByCity);
+async function getDataByCity() {
+
+    const message = document.getElementById("message");
+    message.innerHTML = "Wait for a while...";
+    removeElements();
+    try {
+        const city = document.getElementById("cityName").value;
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
+        const data = await response.json();
+        message.innerHTML = "";
+        console.log(data);
+
+        const timezoneOffset = data.city.timezone;  // In seconds
+        const dailyForecast = data.list.filter(forecast => {
+            const date = new Date((forecast.dt + timezoneOffset) * 1000);
+            const hours = date.getUTCHours();
+            return hours >= 11 && hours <= 13;
+        })
+        console.log(dailyForecast);
+
+
+        let maxTemp = -Infinity;
+        let minTemp = Infinity;
+
+        dailyForecast.forEach(forecast => {
+            forecast.main.feels_like = parseFloat((forecast.main.feels_like - 273.15).toFixed(2));
+            if (forecast.main.temp_max > maxTemp) {
+                maxTemp = forecast.main.temp_max;
+            }
+            if (forecast.main.temp_min < minTemp) {
+                minTemp = forecast.main.temp_min;
+            }
+        });
+
+        console.log(`Max Temp: ${maxTemp}°C, Min Temp: ${minTemp}°C`);
+
+
+        const filteredData = {
+            city: data.city,
+            forecast: dailyForecast,
+            maxTemp: maxTemp - 273.15,
+            minTemp: minTemp - 273.15
+        }
+        console.log(filteredData);
+
+        placeInfoElements(filteredData);
+        getTimeZone(filteredData);
+    } catch (error) {
+        message.innerHTML = `Failed to fetch data: ${error}`;
+    }
+}
 
 latLonBtn.addEventListener("click", getLatLon);
 
@@ -46,9 +101,13 @@ function getLocation() {
 }
 
 async function fetchData(lat, lon) {
+    const message = document.getElementById("message");
+    // console.log(message);
+    message.innerHTML = "Wait for a while..."
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
         const data = await response.json();
+        message.innerHTML = "";
 
         // Get timezone offset from the city data (in seconds)
         const timezoneOffset = data.city.timezone;  // In seconds
@@ -62,9 +121,27 @@ async function fetchData(lat, lon) {
             return hours >= 11 && hours <= 13;
         });
 
+
+        let maxTemp = -Infinity;
+        let minTemp = Infinity;
+
+        dailyForecast.forEach(forecast => {
+            if (forecast.main.temp_max > maxTemp) {
+                maxTemp = forecast.main.temp_max;
+            }
+            if (forecast.main.temp_min < minTemp) {
+                minTemp = forecast.main.temp_min;
+            }
+        });
+
+        console.log(`Max Temp: ${maxTemp}°C, Min Temp: ${minTemp}°C`);
+
+
         const filteredData = {
             city: data.city,  // Include city info
-            forecast: dailyForecast  // Include filtered forecast
+            forecast: dailyForecast, // Include filtered forecast
+            maxTemp: maxTemp,
+            minTemp: minTemp
         };
         console.log(filteredData);
         // Continue processing the filtered data
@@ -96,48 +173,153 @@ function placeInfoElements(filteredData) {
     console.log(weatherIcon.src);
 
     placeName.innerHTML = filteredData.city.name;  // Use city name from data
-    placeName.classList.add("placeName");
+    placeName.classList.add("placeName", "dark:text-white", "text-slate-200");
 
     // Append to DOM
     placeInfo.appendChild(placeName);
     placeInfo.appendChild(descriptionHeading);
     placeInfo.appendChild(weatherIcon);
+    localStorage.setItem("placeInfo", placeInfo);
 
     const dateTime = document.createElement("p");
     dateTime.innerHTML = getTimeZone(filteredData);
-
     const dateTimePara = document.getElementById("dateTime");
     dateTimePara.innerHTML = "";
     dateTimePara.appendChild(dateTime);
+    localStorage.setItem("dateTime", dateTime);
+
+
+    const today = document.getElementById("today");
+    today.innerHTML = getToday();
+
+
+    const windSpeedVal = document.getElementById("windSpeedVal");
+    windSpeedVal.innerHTML = getSpeed(filteredData.forecast[0].wind.speed);
+    const windSpeed = document.getElementById("windSpeed");
+    windSpeed.innerHTML = "Wind speed";
+    const degVal = document.getElementById("degVal");
+    degVal.innerHTML = `${filteredData.forecast[0].wind.deg}°`;
+    const deg = document.getElementById("deg");
+    deg.innerHTML = "Direction";
+    const gustVal = document.getElementById("gustVal");
+    gustVal.innerHTML = getSpeed(filteredData.forecast[0].wind.gust);
+    const gust = document.getElementById("gust");
+    gust.innerHTML = "Gust";
+    // console.log(windSpeedVal, windSpeed, degVal, deg, gustVal, gust);
+
+
+    displayMaxMinTemp(filteredData);
+    displayFeelsLikeTemp(filteredData);
+    displaySunriseSunset(filteredData);
+}
+
+
+function displayMaxMinTemp(filteredData) {
+    console.log(filteredData);
+    const minTempVal = document.getElementById("minTempVal");
+    const minTemp = document.getElementById("minTemp");
+    const humidityVal = document.getElementById("humidityVal");
+    const humidity = document.getElementById("humidity");
+    const maxTempVal = document.getElementById("maxTempVal");
+    const maxTemp = document.getElementById("maxTemp");
+
+
+    minTempVal.innerHTML = `${filteredData.minTemp.toFixed(2)}°C`;
+    minTemp.innerHTML = "MIN";
+
+    humidityVal.innerHTML = `${filteredData.forecast[0].main.humidity}%`;
+    humidity.innerHTML = "Humidity";
+
+    maxTempVal.innerHTML = `${filteredData.maxTemp.toFixed(2)}°C`;
+    maxTemp.innerHTML = "MAX";
+
+
+    // console.log(minTempVal, minTemp, humidityVal, humidity, maxTempVal, maxTemp);
+}
+
+function displayFeelsLikeTemp(filteredData) {
+    const feelsLikeVal = document.getElementById("feelsLikeVal");
+    feelsLikeVal.innerHTML = `${filteredData.forecast[0].main.feels_like}°C`;
+    const feelsLike = document.getElementById("feelsLike");
+    feelsLike.innerHTML = "Feels like";
+
+}
+
+function displaySunriseSunset(filteredData) {
+    const sunriseTime = document.getElementById("sunriseTime");
+    const sunrise = document.getElementById("sunrise");
+    const sunsetTime = document.getElementById("sunsetTime");
+    const sunset = document.getElementById("sunset");
+    // console.log(sunriseTime, sunrise, sunsetTime, sunset);
+
+    const timeZoneOffset = filteredData.city.timezone;
+    const sunriseTimestamp = filteredData.city.sunrise;
+    const sunsetTimestamp = filteredData.city.sunset;
+
+    const sunriseDate = new Date((timeZoneOffset + sunriseTimestamp) * 1000);
+    const sunriseT = formatTime(sunriseDate);
+    sunriseTime.innerHTML = sunriseT;
+    sunrise.innerHTML = "Sunrise";
+
+    const sunsetDate = new Date((timeZoneOffset + sunsetTimestamp) * 1000);
+    // console.log(sunsetDate);
+    const sunsetT = formatTime(sunsetDate);
+    sunsetTime.innerHTML = sunsetT;
+    sunset.innerHTML = "Sunset";
+
+
+    // console.log(timeZoneOffset, sunriseTimestamp, sunsetTimestamp);
+}
+
+function formatTime(date) {
+    const hours = date.getUTCHours().toString().padStart(2, '0');
+    const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+    const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+function removeElements() {
+    const removeAllData = document.querySelectorAll(".remove");
+    removeAllData.forEach(element => {
+        element.innerHTML = "";
+    })
 }
 
 function getTimeZone(filteredData) {
     const timeZoneData = {
-        timeZone: filteredData.city.timezone,
+        timeZone: filteredData.city.timezone, // Offset in seconds from UTC
         dt: filteredData.forecast[0].dt,
-        sunrise: filteredData.city.sunrise,  // `city` object for sunrise and sunset
+        sunrise: filteredData.city.sunrise,
         sunset: filteredData.city.sunset
     };
 
-    const date = new Date(timeZoneData.dt * 1000);
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    // Adjust the timestamp by adding the timezone offset (in milliseconds)
+    const localDate = new Date((timeZoneData.dt + timeZoneData.timeZone) * 1000);
 
+    const year = localDate.getFullYear();
+    const month = (localDate.getMonth() + 1).toString().padStart(2, '0'); // Ensure two digits
+    const day = localDate.getDate().toString().padStart(2, '0'); // Ensure two digits
+    const hours = localDate.getUTCHours().toString().padStart(2, '0'); // Ensure two digits
+    const minutes = localDate.getUTCMinutes().toString().padStart(2, '0'); // Ensure two digits
+    const seconds = localDate.getUTCSeconds().toString().padStart(2, '0'); // Ensure two digits
 
-    // console.log(timeZoneData);
+    // Return the formatted date string
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 
 
+function getSpeed(val) {
+    const value = val * 3.6;
+    const newValue = value.toFixed(2);
+    const newFractionalValue = parseFloat(newValue);
+    return `${newFractionalValue} km/h`;
+}
 
-// const city = 'London';
+function getToday() {
+    const date = new Date();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday"];
 
-// fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-//     .then(response => response.json())
-//     .then(data => console.log(data))
-//     .catch(error => console.error('Error:', error));
+    return days[date.getDay()];
+}
